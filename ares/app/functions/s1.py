79f4b84c03349edc9bdf507f7e93470cf80bc032
Verstrepen Mathis <mathisverstrepen@gmail.com>
2023-02-16 20:42:11 +0000

    fix

--- ares/app/functions/s1.py
+++ ares/app/functions/s1.py
@@ -22,7 +22,6 @@ from io import BytesIO
 import pathlib
 
 
-
 def extract_video_info(data) -> str:
     query, body_data, game, URL = data
     body_data['query'] = game + query
@@ -34,9 +33,10 @@ def extract_video_info(data) -> str:
     # f.close()
 
     renderer = (r_parse['contents']['twoColumnSearchResultsRenderer']['primaryContents']
-                ['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'])
+    ['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'])
 
     return renderer
+
 
 class chapter_scrap():
     def __init__(self, id: str) -> None:
@@ -49,65 +49,67 @@ class chapter_scrap():
         self.vid_meta = json.loads(r_cut_2)
         self.id = id
         self.continuation_token = (self.vid_meta['contents']['twoColumnWatchNextResults']['results']['results']
-                                   ['contents'][-1]['itemSectionRenderer']['contents'][0]['continuationItemRenderer']
-                                   ['continuationEndpoint']['continuationCommand']['token'])
+        ['contents'][-1]['itemSectionRenderer']['contents'][0]['continuationItemRenderer']
+        ['continuationEndpoint']['continuationCommand']['token'])
 
         # f = open(
         #     f'/ares/app/functions/json-{id}.json', 'w')
         # f.write(json.dumps(self.vid_meta))
         # f.close()
-        
+
     def extract_chapter_data(self, text):
         chapter_data = []
         ntext = len(text)
         index = 0
         lastWatchEndpoint = -1
-        
+
         while (index < ntext):
-            
+
             hasNavigationEndpoint = False
             watchEndpoint = -1
-            
+
             nextIndex = index + 1
-            while nextIndex < ntext and not '\n' in text[nextIndex].get('text'): 
+            while nextIndex < ntext and not '\n' in text[nextIndex].get('text'):
                 # logging.debug(text[nextIndex].get('text'))
-                if text[nextIndex].get('navigationEndpoint'): 
-                    if watchEndpoint == -1 : watchEndpoint = text[nextIndex].get('navigationEndpoint').get('watchEndpoint', {}).get('startTimeSeconds')
-                    if watchEndpoint != None : hasNavigationEndpoint = True
-                    
+                if text[nextIndex].get('navigationEndpoint'):
+                    if watchEndpoint == -1: watchEndpoint = text[nextIndex].get('navigationEndpoint').get(
+                        'watchEndpoint', {}).get('startTimeSeconds')
+                    if watchEndpoint != None: hasNavigationEndpoint = True
+
                 nextIndex += 1
-            
+
             if hasNavigationEndpoint and lastWatchEndpoint < watchEndpoint:
                 fullRowElt = []
-                
+
                 fullRowElt.append(text[index].get('text').split('\n')[-1])
-                
+
                 for tmpIndex in range(index + 1, nextIndex):
                     # logging.debug(text[tmpIndex].get('text'))
                     fullRowElt.append(text[tmpIndex].get('text'))
-                    
-                if nextIndex < ntext : fullRowElt.append(text[nextIndex].get('text').split('\n')[0])
-                
+
+                if nextIndex < ntext: fullRowElt.append(text[nextIndex].get('text').split('\n')[0])
+
                 # logging.debug(fullRowElt)
-                
+
                 # if nextIndex < ntext : start_next_string = text[nextIndex].get('text').split('\n')[-1]
-                
+
                 chapter_data.append({
                     'title': self.format_line(fullRowElt),
                     'timestamp': watchEndpoint
                 })
                 lastWatchEndpoint = watchEndpoint
-                
+
                 watchEndpoint = -1
             index = nextIndex
-            
+
         return chapter_data
-        
+
     def by_youtube_data(self):
 
         try:
             chapters = (self.vid_meta['playerOverlays']['playerOverlayRenderer']['decoratedPlayerBarRenderer']
-                        ['decoratedPlayerBarRenderer']['playerBar']['multiMarkersPlayerBarRenderer']['markersMap'][0]['value']['chapters'])
+            ['decoratedPlayerBarRenderer']['playerBar']['multiMarkersPlayerBarRenderer']['markersMap'][0]['value'][
+                'chapters'])
         except Exception:
             chapter_data = None
         else:
@@ -124,7 +126,7 @@ class chapter_scrap():
     def by_video_desc(self):
         try:
             desc = (self.vid_meta['contents']['twoColumnWatchNextResults']['results']['results']
-                    ['contents'][1]['videoSecondaryInfoRenderer']['description']['runs'])
+            ['contents'][1]['videoSecondaryInfoRenderer']['description']['runs'])
         except Exception:
             chapter_data = None
         else:
@@ -148,18 +150,20 @@ class chapter_scrap():
             r_parse = json.loads(r.text)
 
             comments = (r_parse['onResponseReceivedEndpoints']
-                        [1]['reloadContinuationItemsCommand']['continuationItems'])
+            [1]['reloadContinuationItemsCommand']['continuationItems'])
 
             comment_index = 0
-            while comment_index >= 0 and comment_index <= len(comments)-1:
+            while comment_index >= 0 and comment_index <= len(comments) - 1:
                 isComment = comments[comment_index].get('commentThreadRenderer')
-                
+
                 if isComment:
                     com_parts = isComment['comment']['commentRenderer']['contentText']['runs']
                     chapter_data = self.extract_chapter_data(com_parts)
-                        
-                    if len(chapter_data) > 3: comment_index = -1
-                    else : comment_index += 1
+
+                    if len(chapter_data) > 3:
+                        comment_index = -1
+                    else:
+                        comment_index += 1
 
         return chapter_data
 
@@ -168,19 +172,20 @@ class chapter_scrap():
             if lpart:
                 lpart = lpart.split('\n')[0].strip() if (lpart.split('\n')[0]) else lpart.split('\n')[1].strip()
                 part_len = len(lpart)
-                    
+
                 if re.search('[a-zA-Z]', lpart):
-            
+
                     start_index = 0
                     while start_index < part_len and not lpart[start_index].isalpha():
                         start_index += 1
 
                     end_index = part_len - 1
-                    while end_index > start_index and not lpart[end_index].isalpha() and not lpart[end_index].isnumeric():
+                    while end_index > start_index and not lpart[end_index].isalpha() and not lpart[
+                        end_index].isnumeric():
                         end_index -= 1
 
-                    return lpart[start_index:end_index+1]
-                
+                    return lpart[start_index:end_index + 1]
+
 
 class LoggingCursor(psycopg2.extensions.cursor):
     def execute(self, sql, args=None):
@@ -218,11 +223,11 @@ class s1():
         ]
 
         try:
-            self.conn = psycopg2.connect(database="", 
-                                        user="postgres",
-                                        password=os.environ['POSTGRES_PASSWORD'], 
-                                        host="iris",
-                                        port="5432")
+            self.conn = psycopg2.connect(database="",
+                                         user="postgres",
+                                         password=os.environ['POSTGRES_PASSWORD'],
+                                         host="iris",
+                                         port="5432")
         except:
             self.conn = None
 
@@ -248,7 +253,8 @@ class s1():
                 if videoRenderer:
                     videoId = videoRenderer.get('videoId')
                     title = videoRenderer['title']['runs'][0]['text']
-                    thumbnailOverlayTimeStatusRenderer = videoRenderer['thumbnailOverlays'][0].get('thumbnailOverlayTimeStatusRenderer')
+                    thumbnailOverlayTimeStatusRenderer = videoRenderer['thumbnailOverlays'][0].get(
+                        'thumbnailOverlayTimeStatusRenderer')
                     if thumbnailOverlayTimeStatusRenderer:
                         duration = thumbnailOverlayTimeStatusRenderer['text']['simpleText']
                         if final.get(videoId):
@@ -274,14 +280,14 @@ class s1():
         return chapter_data
 
     def downloader(self, vidID: str, gameID: int) -> list:
-        
+
         dir = f'/bacchus/audio/{gameID}'
-        try :
+        try:
             shutil.rmtree(dir)
-        except : pass
+        except:
+            pass
         os.mkdir(dir)
-            
-        
+
         URL = f'http://www.youtube.com/watch?v={vidID}'
 
         ydl_opts = {
@@ -298,94 +304,95 @@ class s1():
             ydl.download([URL])
 
         return vid_dur
-    
+
     def format_audio(gameID: int, audioID: str, audioIndex: int, r_games):
         audio = AudioSegment.from_file(f"/bacchus/audio/{gameID}/{audioID}.m4a")
         audioLength = len(audio)
         audioMetadata = []
-        
+
         dir = f"/bacchus/audio/{gameID}/{audioID}"
-        if (not os.path.isdir(dir)) : os.makedirs(dir)
+        if (not os.path.isdir(dir)): os.makedirs(dir)
         files = glob.glob(f"/bacchus/audio/{gameID}/{audioID}/*")
         for f in files:
             os.remove(f)
-        
+
         currentTimecode = 0
-        while currentTimecode < audioLength - 10*1000:
+        while currentTimecode < audioLength - 10 * 1000:
             currentTimecodeDelay = currentTimecode if currentTimecode == 0 else currentTimecode - 100
-            cutAudio = audio[currentTimecodeDelay:currentTimecode + (10*1000 + 100)]
-            
-            wavIO=BytesIO()
+            cutAudio = audio[currentTimecodeDelay:currentTimecode + (10 * 1000 + 100)]
+
+            wavIO = BytesIO()
             cutAudio.export(wavIO, format="mp3")
             pathlib.Path(f"/bacchus/audio/{gameID}/{audioID}/{currentTimecode}").write_bytes(wavIO.getbuffer())
-            
+
             audioMetadata.append(currentTimecode)
-            currentTimecode += 10*1000
-                
+            currentTimecode += 10 * 1000
+
         cutAudio = audio[currentTimecode - 100:audioLength]
-        wavIO=BytesIO()
+        wavIO = BytesIO()
         cutAudio.export(wavIO, format="mp3")
         pathlib.Path(f"/bacchus/audio/{gameID}/{audioID}/{currentTimecode}").write_bytes(wavIO.getbuffer())
-        audioMetadata.append(currentTimecode) 
-        
+        audioMetadata.append(currentTimecode)
+
         r_games.json().set(f"g:{gameID}", f"$.album[0].track[{audioIndex}].chunkMeta", audioMetadata)
-        
+
         logging.debug(f'Format track {audioID}')
 
-    def file_formater(self, gameID: int, chapters: list, vid_dur:int, r_games) -> list:
-        
+    def file_formater(self, gameID: int, chapters: list, vid_dur: int, r_games) -> list:
+
         r_games.json().set(f"g:{gameID}", "$.album", [])
-        
+
         with self.conn.cursor(cursor_factory=LoggingCursor) as curs:
 
             game_name_slug = slugify(r_games.json().get(f"g:{gameID}", '$.slug')[0] + " full album")
             subprocess_list = []
             tracklist = []
-            
+
             for i in range(len(chapters)):
-                
                 file_uuid = uuid.uuid4().hex
                 title_slug = slugify(chapters[i]['title'])
                 start = chapters[i]['timestamp']
-                end = vid_dur if i >= len(chapters)-1 else chapters[i+1]['timestamp']
+                end = vid_dur if i >= len(chapters) - 1 else chapters[i + 1]['timestamp']
 
-                query = sql.SQL("INSERT INTO iris.track (game_id, title, slug, file, view_count, like_count, length) VALUES (%s,%s,%s,%s,%s,%s,%s) RETURNING id;")
-                data = (gameID, chapters[i]['title'], title_slug, file_uuid, 0, 0, end-start)
+                query = sql.SQL("INSERT INTO iris.track (game_id, title, slug, file, view_count, like_count, length) "
+                                "VALUES (%s,%s,%s,%s,%s,%s,%s) RETURNING id;")
+                data = (gameID, chapters[i]['title'], title_slug, file_uuid, 0, 0, end - start)
                 curs.execute(query, data)
                 track_id = curs.fetchall()[0][0]
-                
+
                 query = sql.SQL("INSERT INTO iris.album (game_id, track_id, name, slug) VALUES (%s,%s,%s,%s);")
                 data = (gameID, track_id, "Full Album", game_name_slug)
                 curs.execute(query, data)
 
                 p = subprocess.Popen(['ffmpeg', '-loglevel', 'error', '-i', f'/bacchus/audio/{gameID}/temp.m4a', '-ss',
-                                str(timedelta(seconds=start)), '-to', str(timedelta(seconds=end)), '-c', 'copy', '-y', f'/bacchus/audio/{gameID}/{file_uuid}.m4a'], shell=False,
-                                stdin=None, stdout=None, stderr=None, close_fds=True)
+                                      str(timedelta(seconds=start)), '-to', str(timedelta(seconds=end)), '-c', 'copy',
+                                      '-y', f'/bacchus/audio/{gameID}/{file_uuid}.m4a'], shell=False,
+                                     stdin=None, stdout=None, stderr=None, close_fds=True)
                 subprocess_list.append(p)
 
                 tracklist.append({
                     'id': track_id,
                     'title': chapters[i]['title'],
-                    'slug' : title_slug,
+                    'slug': title_slug,
                     'file': file_uuid,
-                    'view_count' : 0,
+                    'view_count': 0,
                     'like_count': 0,
-                    'length': end-start
+                    'length': end - start
                 })
 
         [p.wait() for p in subprocess_list]
-        
+
         r_games.json().arrinsert(f"g:{gameID}", "$.album", 0, {
-            "name" : "Full Album",
-            "slug" : game_name_slug,
-            "track" : tracklist
+            "name": "Full Album",
+            "slug": game_name_slug,
+            "track": tracklist
         })
-        
-        for index, track in enumerate(tracklist) :
-            Thread(target=s1.format_audio, args=(gameID, track['file'], index, r_games, )).start()
+
+        for index, track in enumerate(tracklist):
+            Thread(target=s1.format_audio, args=(gameID, track['file'], index, r_games,)).start()
 
         os.remove(f"/bacchus/audio/{gameID}/temp.m4a")
-        
+
         self.conn.commit()
 
         return tracklist
--- ares/app/main.py
+++ ares/app/main.py
@@ -94,7 +94,6 @@ async def add_process_time_header(request, call_next):
 
 @ares.get("/matching_games")
 def get_best_matching_games(game: str, token: str = Depends(oauth2_scheme)) -> dict:
-
     auth(token)
 
     logging.info("Searching matching games for input [%s].", game)
@@ -108,11 +107,10 @@ def get_best_matching_games(game: str, token: str = Depends(oauth2_scheme)) -> d
 @ares.post("/new_game")
 @limiter.limit("60/minute")
 def push_new_game(request: Request, gameID: int, token: str = Depends(oauth2_scheme)) -> dict:
-
     auth(token)
 
     logging.info("Obtaining the metadata of the game ID [%s].", gameID)
- 
+
     IGDB_cli = IGDB(r_glob)
     game_data = IGDB_cli.new_game(gameID)
 
@@ -129,7 +127,6 @@ def push_new_game(request: Request, gameID: int, token: str = Depends(oauth2_sch
 @ares.delete("/del/game")
 @limiter.limit("60/minute")
 def delete_game(request: Request, gameID: int, token: str = Depends(oauth2_scheme)) -> None:
-
     auth(token)
 
     iris_cli = iris(r_glob, r_games)
@@ -141,14 +138,13 @@ def delete_game(request: Request, gameID: int, token: str = Depends(oauth2_schem
 @ares.get("/s1/match")
 @limiter.limit("60/minute")
 def get_best_matching_games_s1(request: Request, gameID: int, token: str = Depends(oauth2_scheme)) -> dict:
-
     auth(token)
 
     logging.info("Searching best matching data from Source 1 for game ID [%s].", gameID)
 
     res_name = r_games.json().get(f"g:{gameID}", "$.name")
     if res_name:
-        res_s1_match = r_games.json().get(f"g:{gameID}", "$.s1.match") 
+        res_s1_match = r_games.json().get(f"g:{gameID}", "$.s1.match")
         if not res_s1_match:
             s1_client = s1()
             res = s1_client.best_match(res_name[0])
@@ -168,7 +164,6 @@ def get_best_matching_games_s1(request: Request, gameID: int, token: str = Depen
 @ares.get("/s1/chapter")
 @limiter.limit("60/minute")
 def get_chapter_s1(request: Request, id: str, gameID: int, token: str = Depends(oauth2_scheme)) -> dict:
-
     auth(token)
 
     logging.info("Searching chapters for data from Source 1 for game ID [%s].", gameID)
@@ -198,8 +193,7 @@ def get_chapter_s1(request: Request, id: str, gameID: int, token: str = Depends(
 
 @ares.get("/s1/download")
 @limiter.limit("60/minute")
-async def get_download_s1(request: Request, vidID: str, gameID: int, token: str = Depends(oauth2_scheme)) -> None:
-
+async def get_download_s1(request: Request, vidID: str, gameID: int, token: str = Depends(oauth2_scheme)) -> dict:
     auth(token)
 
     logging.info("Downloading audio data from Source 1 for game ID [%s].", gameID)
@@ -217,8 +211,7 @@ async def get_download_s1(request: Request, vidID: str, gameID: int, token: str 
 
 @ares.get("/s1/format_file")
 @limiter.limit("60/minute")
-async def get_file_format_s1(request: Request, gameID: int, vid_dur: int, token: str = Depends(oauth2_scheme)) -> None:
-
+async def get_file_format_s1(request: Request, gameID: int, vid_dur: int, token: str = Depends(oauth2_scheme)) -> dict:
     auth(token)
 
     logging.info("Formating audio data from Source 1 for game ID [%s].", gameID)
@@ -236,7 +229,6 @@ async def get_file_format_s1(request: Request, gameID: int, vid_dur: int, token:
 @ares.get("/r1/new")
 # @limiter.limit("5/minute")
 async def add_new_user_redis(userData: object) -> None:
-
     print(userData)
 
     userData = json.loads(userData)
@@ -254,7 +246,6 @@ async def add_new_user_redis(userData: object) -> None:
 
 @ares.get("/r1/get")
 def get_user_redis(data: object) -> None:
-
     data = json.loads(data)
     elements = data["el"]
 
@@ -273,129 +264,138 @@ def get_user_redis(data: object) -> None:
 
 @ares.get("/v1/game")
 # @limiter.limit("60/minute")
-async def get_game_data(request: Request, gID: int, labels: list[str] = Query(default=['base']), debug: bool = False, forceDB: bool = False) -> dict:
-    
+async def get_game_data(request: Request, gID: int, labels: list[str] = Query(default=['base']), debug: bool = False,
+                        forceDB: bool = False) -> dict:
     fData = {}
     debugData = {}
 
     for label in labels:
-        if (label == 'base'):
+        if label == 'base':
             start = timer()
             res = iris_cli.get_base_game_data(gID, forceDB)
             end = timer()
             fData['base'] = res
-            if debug: debugData['base'] = (end-start)*1000
+            if debug: debugData['base'] = (end - start) * 1000
 
-        elif (label in ['artworks', 'cover', 'screenshots']):
+        elif label in ['artworks', 'cover', 'screenshots']:
             start = timer()
             res = iris_cli.get_media_game_data(gID, label)
             end = timer()
             fData[label] = res
-            if debug: debugData[label] = (end-start)*1000
-            
-        elif (label == 'alternative_name'):
+            if debug: debugData[label] = (end - start) * 1000
+
+        elif label == 'alternative_name':
             start = timer()
             res = iris_cli.get_alternative_name_game_data(gID)
             end = timer()
             fData[label] = res
-            if debug: debugData[label] = (end-start)*1000
-            
-        elif (label == 'album'):
+            if debug: debugData[label] = (end - start) * 1000
+
+        elif label == 'album':
             start = timer()
             res = iris_cli.get_album_game_data(gID, forceDB)
             end = timer()
             fData[label] = res
-            if debug: debugData[label] = (end-start)*1000
-            
-        elif (label == 'involved_companies'):
+            if debug: debugData[label] = (end - start) * 1000
+
+        elif label == 'involved_companies':
             start = timer()
             res = iris_cli.get_involved_companies_game_data(gID)
             end = timer()
             fData[label] = res
-            if debug: debugData[label] = (end-start)*1000
-            
-        elif (label in ['dlcs', 'expansions', 'expanded_games', 'similar_games', 'standalone_expansions']):
+            if debug: debugData[label] = (end - start) * 1000
+
+        elif label in ['dlcs', 'expansions', 'expanded_games', 'similar_games', 'standalone_expansions']:
             start = timer()
             res = iris_cli.get_extra_content_game_data(gID, label)
             end = timer()
             fData[label] = res
-            if debug: debugData[label] = (end-start)*1000
+            if debug: debugData[label] = (end - start) * 1000
 
-        elif (label == 'genre'):
+        elif label == 'genre':
             start = timer()
             res = iris_cli.get_genre_game_data(gID)
             end = timer()
             fData[label] = res
-            if debug: debugData[label] = (end-start)*1000
-            
-        elif (label == 'theme'):
+            if debug: debugData[label] = (end - start) * 1000
+
+        elif label == 'theme':
             start = timer()
             res = iris_cli.get_theme_game_data(gID)
             end = timer()
             fData[label] = res
-            if debug: debugData[label] = (end-start)*1000
-            
-        elif (label == 'keyword'):
+            if debug: debugData[label] = (end - start) * 1000
+
+        elif label == 'keyword':
             start = timer()
             res = iris_cli.get_keyword_game_data(gID)
             end = timer()
             fData[label] = res
-            if debug: debugData[label] = (end-start)*1000
-            
+            if debug: debugData[label] = (end - start) * 1000
+
     return {"debug_data": debugData, "gameID": gID, "data": fData}
+
 
 @ares.get("/v1/game/random/{limit}")
 # @limiter.limit("60/minute")
-async def get_random_games(request: Request, labels: list[str] = Query(default=['base']), limit: int = Path(0, title="Number of random games", gt=0, le=1000), debug: bool = False, forceDB: bool = False) -> dict:
-    
+async def get_random_games(request: Request, labels: list[str] = Query(default=['base']),
+                           limit: int = Path(0, title="Number of random games", gt=0, le=1000), debug: bool = False,
+                           forceDB: bool = False) -> dict:
     # logging.info(labels)
     data = []
     randID: list = iris_cli.getRandomCompleteGameIDs(limit)
     for gameID in randID:
         data.append(await get_game_data(request, gameID[0], labels, debug, forceDB))
-    
+
     return {"data": data}
+
 
 @ares.get("/v1/game/top/rating/{limit}")
 # @limiter.limit("60/minute")
-async def get_top_rated_games(request: Request, labels: list[str] = Query(default=['base']), limit: int = Path(0, title="Number of top rating games", gt=0, le=1000), debug: bool = False, forceDB: bool = False) -> dict:
-    
+async def get_top_rated_games(request: Request, labels: list[str] = Query(default=['base']),
+                              limit: int = Path(0, title="Number of top rating games", gt=0, le=1000),
+                              debug: bool = False, forceDB: bool = False) -> dict:
     # logging.info(labels)
     data = []
     topRateIDs: list = iris_cli.getTopRatedGameIDs(limit)
     for gameID in topRateIDs:
         data.append(await get_game_data(request, gameID[0], labels, debug, forceDB))
-    
+
     return {"data": data}
+
 
 @ares.get("/v1/collection/top/{limit}")
 # @limiter.limit("60/minute")
-async def get_top_rated_collection(request: Request, labels: list[str] = Query(default=['base']), limit: int = Path(0, title="Number of top rating collection", gt=0, le=1000), debug: bool = False, forceDB: bool = False) -> dict:
-    
+async def get_top_rated_collection(request: Request, labels: list[str] = Query(default=['base']),
+                                   limit: int = Path(0, title="Number of top rating collection", gt=0, le=1000),
+                                   debug: bool = False, forceDB: bool = False) -> dict:
     # logging.info(labels)
     data = {}
     topRateIDs: list = iris_cli.getTopRatedCollectionIDs(limit)
-    
+
     for gameID, collectionID, collectionName in topRateIDs:
         data.setdefault(collectionID, {})['collectionName'] = collectionName
-        data[collectionID].setdefault('gameData', []).append(await get_game_data(request, gameID, labels, debug, forceDB))
+        data[collectionID].setdefault('gameData', []).append(
+            await get_game_data(request, gameID, labels, debug, forceDB))
 
     return {"data": data}
 
+
 @ares.get("/v1/collection/{collectionID}")
 # @limiter.limit("60/minute")
-async def get_collection_by_id(request: Request, labels: list[str] = Query(default=['base']), collectionID: int = Path(0, title="Collection ID"), debug: bool = False, forceDB: bool = False) -> dict:
-    
+async def get_collection_by_id(request: Request, labels: list[str] = Query(default=['base']),
+                               collectionID: int = Path(0, title="Collection ID"), debug: bool = False,
+                               forceDB: bool = False) -> dict:
     logging.info(labels)
     collectionData: list = iris_cli.getCollectionData(collectionID)
     collectionGameID: list = iris_cli.getGameIDofCollection(collectionID)
-    
+
     data = {
-        "collection" : {
+        "collection": {
             "name": collectionData[0],
             "slug": collectionData[1]
         },
-        "games" : {}
+        "games": {}
     }
     logging.info(data)
     for game in collectionGameID:
@@ -403,10 +403,11 @@ async def get_collection_by_id(request: Request, labels: list[str] = Query(defau
 
     return {"data": data}
 
+
 @ares.get("/v1/game/search/{searchText}")
 # @limiter.limit("60/minute")
-async def get_collection_by_id(request: Request, searchText: str = Path(0, title="Search game text"), debug: bool = False) -> dict:
-    
+async def get_collection_by_id(request: Request, searchText: str = Path(0, title="Search game text"),
+                               debug: bool = False) -> dict:
     searchResults = iris_cli.searchGameByName(searchText)
-    
+
     return {"data": searchResults}
